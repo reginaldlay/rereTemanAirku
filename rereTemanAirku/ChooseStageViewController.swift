c61cb6cae833490f0765a2e09941382588d4e52b
Reginald <reginaldlay777@gmail.com>
2022-04-11 17:31:30 +0700

    nambahin Choose Stage View Controller dan Storyboard nya

---
+++ rereTemanAirku/ChooseStageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ChooseStageViewController.swift
+//  rereTemanAirku
+//
+//  Created by Reginald Lay on 11/04/22.
+//
+
+import UIKit
+
+class ChooseStageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
